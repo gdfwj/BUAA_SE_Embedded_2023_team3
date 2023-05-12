c9e200a249fd2f13da1ed8eebf41b283ecf0f072
李国玮 <20373159@buaa.edu.cn>
2023-05-12 21:38:39 +0800

    快弄完了

--- src/patch_embedding/scripts/webServer.py
+++ src/patch_embedding/scripts/webServer.py
@@ -7,12 +7,16 @@
 """
 import asyncio
 import re
+import rospy
 
 import websockets
 import controller
 #服务端ip地址、端口号
 ip = 'localhost'
 port = 8765
+class webServer:
+    def __init__(self, controller):
+        controller
 #消息格式：和前后端url匹配，
 #服务端响应函数
 async def echo(websocket, path):
@@ -35,12 +39,18 @@ async def echo(websocket, path):
                 break
             controller.getController().edit_mark(int(result.group()))
             message = "I got your message: {}".format(message)
-        elif re.match("mark/save:", message):
+        # elif re.match("mark/save:", message):
 
         else :
             message = "Invalid message!!!"
         await websocket.send(message)
 
-# 注册服务端
-asyncio.get_event_loop().run_until_complete(websockets.serve(echo, ip, port))
-asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    rospy.init_node("")
+    rospy.wait_for_service('')
+    client = rospy.ServiceProxy('/control/web', )
+    # 注册服务端
+    asyncio.get_event_loop().run_until_complete(websockets.serve(echo, ip, port))
+    asyncio.get_event_loop().run_forever()
+
+
