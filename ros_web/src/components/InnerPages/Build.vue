313929fbeb93eb5b364cd447973ce9825c84b0ff
Gao Boyu <tidreme@outlook.com>
2023-06-11 11:09:36 +0800

    fix

--- ros_web/src/components/InnerPages/Build.vue
+++ ros_web/src/components/InnerPages/Build.vue
@@ -33,7 +33,7 @@
             <el-row>
 <!--             地图图片-->
               <el-col>
-                <el-image :src="require(`@/assets/`+ map.map_id + `.png`)" style="width: 100%"></el-image>
+                <el-image :src="require(`//home//jinghongbin//SE//team03-project//src//patch_embedding//maps//map`+ map.map_id + `.png`)" style="width: 100%"></el-image>
               </el-col>
             </el-row>
             <el-row style="padding-top: 10px">
@@ -106,7 +106,7 @@
         <el-row>
           <el-image
             style="width: 50%; height: 50%; padding-left: 25%"
-            :src="require(`@/assets/`+ this.map_id + `.png`)">
+            :src="require(`//home//jinghongbin//SE//team03-project//src//patch_embedding//maps//map`+ this.map_id + `.png`)">
           </el-image>
         </el-row>
 
